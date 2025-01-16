77d7b577dde333a7a7d894b92cd3f2777d70ee5b
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-01-16 10:17:21 +0800

    first commit

---
+++ resources/js/Components/NavLink.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+import { usePage } from "@inertiajs/vue3";
+
+const isActive = (route) => {
+    return usePage().url.includes(route);
+};
+
+defineProps({
+    href: {
+        type: String,
+        required: true,
+    },
+    icon: {
+        required: true,
+    },
+});
+
+
+</script>
+
+<template>
+    <Link
+        :href="href"
+        class="flex items-center gap-3 rounded-l-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+        :class="{ 'text-primary bg-primary/10': isActive(href) }"
+    >
+        <component :is="icon" class="h-4 w-4" />
+        <slot></slot>
+    </Link>
+</template>
