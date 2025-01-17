770a2ee6d9faf7680b54ed0e3e6f2a836256f7f9
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-01-17 18:52:17 +0800

    updated the columns for users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,8 +18,11 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'work_phone',
+        'work_email',
         'password',
     ];
 
