3da52926dfd028e37fc7b31e17af1d0fbeef6dbb
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-03-10 12:55:17 +0800

    updated the function on offices final average controller

--- app/Http/Controllers/OfficeFinalAverageController.php
+++ app/Http/Controllers/OfficeFinalAverageController.php
@@ -12,7 +12,6 @@ class OfficeFinalAverageController extends Controller
     public function index()
     {
         $offices = Office::with(['user_tasks'])->latest()
-            ->latest()
             ->paginate(10)
             ->withQueryString()
             ->through(fn($office) => [
@@ -20,7 +19,13 @@ class OfficeFinalAverageController extends Controller
                 'name' => $office->name,
                 'office_code' => $office->office_code,
                 'final_average' => $office->user_tasks->count() > 0
-                    ? number_format($office->user_tasks->sum(fn($user_task) => ($user_task->q + $user_task->t + $user_task->e) / 3) / $office->user_tasks->count(), 2)
+                    ? number_format($office->user_tasks->sum(function ($user_task) {
+                        $count = 0;
+                        if ($user_task->q > 0) $count++;
+                        if ($user_task->t > 0) $count++;
+                        if ($user_task->e > 0) $count++;
+                        return $count > 0 ? ($user_task->q + $user_task->t + $user_task->e) / $count : 0;
+                    }) / $office->user_tasks->count(), 2)
                     : 0
             ]);
 
