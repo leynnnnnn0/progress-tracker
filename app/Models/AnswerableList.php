21cb080bc4a49c263532062ca47805405d2f4e46
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-03-14 12:19:54 +0800

    implemented auditable for all models

--- app/Models/AnswerableList.php
+++ app/Models/AnswerableList.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class AnswerableList extends Model
+class AnswerableList extends Model implements Auditable
 {
     /** @use HasFactory<\Database\Factories\AnswerableListFactory> */
-    use HasFactory;
+    use HasFactory, \OwenIt\Auditing\Auditable;
 
 
     protected $fillable = [
