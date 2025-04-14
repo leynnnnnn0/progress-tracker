fcf324d0e7f240c7a261105414589ea9739db386
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-04-14 14:23:22 +0800

    change the position of logout button

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -218,6 +218,18 @@ const toggleMobileSidebar = () => {
                                         >Settings</span
                                     >
                                 </NavLink>
+                                <button
+                                    @click="logout"
+                                    class="flex items-center gap-2 px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                                    :class="{
+                                        'justify-center': isSidebarCollapsed,
+                                    }"
+                                >
+                                    <LogOut class="size-5" />
+                                    <span v-if="!isSidebarCollapsed"
+                                        >Logout</span
+                                    >
+                                </button>
                             </nav>
                         </section>
                         <button
